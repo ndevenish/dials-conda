59794faf3f35ea9101695496590cc4d5bde343e5
Nicholas Devenish <ndevenish@gmail.com>
2019-10-04 23:24:31 +0100

    Progress

--- build.sh
+++ build.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 # $PYTHON setup.py
-echo "Building"
+
+set -e
+
+step() {
+    printf "\e[1m${*}\e[0m\n"
+}
+if [[ -z "$PYTHON" ]]; then
+    export PYTHON=$(which python3)
+fi
+step "Generating build files"
+( cd tbxtools && pwd && $PYTHON setup.py develop; )
+pwd
+tbx2cmake modules
+[[ -f modules/CMakeLists.txt ]] || ( cd modules && ln -s cmake/CMakeLists.txt .; )
+
+step "Generating Build"
+[[ -f "_build/build.ninja" ]] || (
+    mkdir -p _build && cd _build
+    # Extra python args to ensure picks up conda python
+    cmake ../modules -GNinja \
+        -DBOOST_ROOT=$CONDA_PREFIX \
+        -DPython_ROOT_DIR=$CONDA_PREFIX -DPython_FIND_STRATEGY=LOCATION
+)
+
+step "Build"
+(
+    cd _build
+    # Dependency resolution isn't completely derived so do generation first
+    ninja scitbx_refresh
+    ninja
+)
+
+# step ""
