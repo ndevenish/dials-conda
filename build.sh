f3de1ddf8a44ffe3f7fb12b4dd1b67d5847b1b6e
Nicholas Devenish <ndevenish@gmail.com>
2019-10-07 12:38:24 +0100

    Add a develop mode flag for testing

--- build.sh
+++ build.sh
@@ -3,26 +3,55 @@
 
 set -e
 
-step() {
+# Flag for debugging the build - will try to softlink instead of final install
+export INSTALL_DEVELOP=1
+
+stage() {
     printf "\e[1m${*}\e[0m\n"
 }
+step() {
+    printf "%-74s[" "$*"
+    set +e
+}
+poststep() {
+    if [[ $? -eq 0 ]]; then
+        printf " \e[32mOK\e[0m ]\n"
+        set -e
+        return 0
+    else
+      printf "\e[31mFAIL\e[0m]\n"
+        set -e
+      return 1
+    fi
+}
+
 if [[ -z "$PYTHON" ]]; then
     export PYTHON=$(which python3)
 fi
-step "Generating build files"
+
+
+stage "Generating build files"
 ( cd tbxtools && pwd && $PYTHON setup.py develop; )
 pwd
 tbx2cmake modules
+step "Create CMakeLists"
 [[ -f modules/CMakeLists.txt ]] || ( cd modules && ln -s cmake/CMakeLists.txt .; )
+poststep
 # While we have a simple non-resolving cmakelist disable things
 # fast_linalg depends on lapack and isn't built by default
+step "Disable fast_linalg"
 sed -ie 's;add_subdirectory(cctbx_project/fast_linalg);#add_subdirectory(cctbx_project/fast_linalg);' modules/autogen_CMakeLists.txt
+poststep
 # lstbx benchmark depends on fast_linalg in some undeclared way
+step "Disable benchmarks"
 sed -ie 's;add_subdirectory(benchmarks);#add_subdirectory(benchmarks);' modules/cctbx_project/scitbx/lstbx/CMakeLists.txt
+poststep
 # Replace libtbx.env loading with one that works via entrypoints
+step "Replace libtbx environment"
 cp patches/env_generic.py modules/cctbx_project/libtbx/load_env.py
+poststep
 
-step "Generating Build"
+stage "Generating Build"
 [[ -f "_build/build.ninja" ]] || (
     mkdir -p _build && cd _build
     # Extra python args to ensure picks up conda python
@@ -31,7 +60,7 @@ step "Generating Build"
         -DPython_ROOT_DIR=$CONDA_PREFIX -DPython_FIND_STRATEGY=LOCATION
 )
 
-step "Build"
+stage "Build"
 (
     cd _build
     # Dependency resolution isn't completely derived so do generation first
@@ -39,4 +68,7 @@ step "Build"
     ninja
 )
 
-# step ""
+stage "Install"
+export LIBTBX_BUILD=$(pwd)/_build
+bash ./setup.sh
+
--- setup.sh
+++ setup.sh
@@ -9,3 +9,10 @@ ln -fs $(ls -d ../modules/*/ | grep -v cmake | grep -v cctbx_project) .
 ln -fs $(ls -d ../modules/cctbx_project/*/ | grep -v dxtbx) .
 ln -fs ../setup.py
 
+# Run the install
+if [[ -n "$INSTALL_DEVELOP" ]]; then
+    $PYTHON -mpip install -e .
+else
+    $PYTHON -mpip install .
+fi
+
