3316ad8ee41394ac67cf8951b8670b2e8908f8b1
Nicholas Devenish <ndevenish@gmail.com>
2020-09-21 15:05:18 +0100

    Add more generic environment

--- patches/env_generic.py
+++ patches/env_generic.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+import libtbx
 import entrypoints
 import pkgutil
+
 
 class Environment:
     """
@@ -11,13 +13,31 @@ class Environment:
     that shouldn't matter anyway.
     """
 
-    def under_base(self, path):
+    def under_base(self, path: str) -> str:
         # Not entirely sure what is appropriate here; we don't have the
         # concept of a base dir.
         return str(Path("/no/defined/canonical/base") / path)
 
-    def dist_path(self, module):
+    def under_build(self, path: str) -> str:
+        """No concept of build, except the in-place site-packages
+
+        For now, return a dummy but leave open to returning site packages.
+        """
+        # return Path(libtbx.__path__[0]).parent / path
+        return str(Path("/no/defined/canonical/build") / path)
+
+    def dist_path(self, module: str) -> str:
         entrypoint = entrypoints.get_single("libtbx.module", module)
         # Work out where this will be, without importing
         package = pkgutil.get_loader(entrypoint.module_name)
         return str(Path(package.path).parent)
+
+    def has_module(self, module: str) -> bool:
+        try:
+            entrypoint = entrypoints.get_single("libtbx.module", module)
+            return True
+        except entrypoint.NoSuchEntryPoint:
+            return False
+
+
+libtbx.env = Environment()
